f1fb50b4601ddb45afeab5ca8765273d80cfba06
Stefan Simon <stefan.j.c.simon@gmail.com>
2024-12-30 15:59:08 +0100

    [FIX] Exclusion of paths not correct

--- auth_lib/src/middleware.rs
+++ auth_lib/src/middleware.rs
@@ -1,6 +1,13 @@
-use std::{future::{ready, Ready}, marker::PhantomData, rc::Rc};
+use std::{
+    future::{ready, Ready},
+    marker::PhantomData,
+    rc::Rc,
+};
 
-use actix_web::{dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform}, Error, HttpMessage};
+use actix_web::{
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+    Error, HttpMessage,
+};
 use futures::future::LocalBoxFuture;
 use regex::Regex;
 use serde::de::DeserializeOwned;
@@ -10,42 +17,47 @@ use crate::{AuthToken, GetAuthenticatedUserFromRequest, UnauthorizedError};
 
 const PATH_MATCHER_ANY_ENCODED: &str = "%2A"; // to match *
 
-/// PathMatcher is used to secure specific paths or to exclude paths from authenticatio
-/// exclude: exclude the path_list from authentication if true, else handles path_list as included for authentication.
-/// path_list: Vec of paths. The path_list may include wildcards like "/api/user/*"
+/// PathMatcher is used to match specific paths or to exclude paths from matching
+/// is_exclusion_list: if true, the entries of path_list will not match otherwise only the entries will match.
+/// path_list: List of paths you wish to exclude or include (see: is_exclusion_list). The path_list may include wildcards like "/api/user/*"
 pub struct PathMatcher {
-    exclude: bool,
-    path_regex_list: Vec<(&'static str, Regex)>
+    is_exclusion_list: bool,
+    path_regex_list: Vec<(&'static str, Regex)>,
 }
 
 impl PathMatcher {
-
     /// All routes are secured except everything starting with /login or register (e.g.: /login?error=true, /login-anything, /register, /register-error)
     /// **Warning:** currently it would left a routes like /register-private-thing/user/123 unsecure
     pub fn default() -> Self {
         Self::new(vec!["/login*", "/register*"], true)
     }
 
-    pub fn new(path_list: Vec<&'static str>, exclude: bool) -> Self {
+    pub fn new(path_list: Vec<&'static str>, is_exclusion_list: bool) -> Self {
         let mut path_regex_list = Vec::new();
         for pattern in path_list.into_iter() {
-            
-            path_regex_list.push((pattern, Regex::new(&transform_to_encoded_regex(pattern)).unwrap()));
+            path_regex_list.push((
+                pattern,
+                Regex::new(&transform_to_encoded_regex(pattern)).unwrap(),
+            ));
         }
         Self {
-            exclude,
+            is_exclusion_list,
             path_regex_list,
         }
     }
 
     pub fn matches(&self, path: &str) -> bool {
         let encoded_path = transform_to_encoded_regex(path);
-        self.path_regex_list
-        .iter()
-        .any(|p| {
+        let mut path_regex_iter = self.path_regex_list.iter();
+        if self.is_exclusion_list {
+            return path_regex_iter.all(|p| !p.1.is_match(&encoded_path));
+        } else {
+            return path_regex_iter.any(|p| {
                 println!("check: {} against: {}", path, p.1.as_str());
-                (p.1.is_match(&encoded_path) && !self.exclude) || !p.1.is_match(&encoded_path)
+
+                p.1.is_match(&encoded_path)
             })
+        }
     }
 }
 
@@ -55,20 +67,20 @@ fn transform_to_encoded_regex(input: &str) -> String {
     valid_regex
 }
 
-pub struct AuthMiddleware<AuthProvider, U> 
-where 
+pub struct AuthMiddleware<AuthProvider, U>
+where
     AuthProvider: GetAuthenticatedUserFromRequest<U>,
-    U: DeserializeOwned
+    U: DeserializeOwned,
 {
     auth_provider: Rc<AuthProvider>,
     path_matcher: Rc<PathMatcher>,
-    user_type: PhantomData<U>
+    user_type: PhantomData<U>,
 }
 
-impl<AuthProvider, U> AuthMiddleware<AuthProvider, U> 
-where 
+impl<AuthProvider, U> AuthMiddleware<AuthProvider, U>
+where
     AuthProvider: GetAuthenticatedUserFromRequest<U>,
-    U: DeserializeOwned
+    U: DeserializeOwned,
 {
     pub fn new(auth_provider: AuthProvider, path_matcher: PathMatcher) -> Self {
         AuthMiddleware {
@@ -80,14 +92,14 @@ where
 }
 
 pub struct AuthMiddlewareInner<S, AuthProvider, U>
-where 
+where
     AuthProvider: GetAuthenticatedUserFromRequest<U>,
-    U: DeserializeOwned
+    U: DeserializeOwned,
 {
     service: S,
     auth_provider: Rc<AuthProvider>,
     path_matcher: Rc<PathMatcher>,
-    user_type: PhantomData<U>
+    user_type: PhantomData<U>,
 }
 
 impl<S, B, AuthProvider, U> Service<ServiceRequest> for AuthMiddlewareInner<S, AuthProvider, U>
@@ -105,7 +117,7 @@ where
     forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        println!("Session authentication {}", req.path());
+        println!("Check authentication {}", req.path());
 
         let request_path = req.request().path();
         if self.path_matcher.matches(request_path) {
@@ -114,19 +126,17 @@ where
                     let token = AuthToken::new(user);
                     let mut extensions = req.extensions_mut();
                     extensions.insert(token);
-                },
-                Err(_) =>  {
+                }
+                Err(_) => {
                     // TODO: should use appropriate logging
                     println!("Authenticated route but no authenticated user found..");
-                    return Box::pin( async {
-                        Err( UnauthorizedError::default().into() )
-                    });
+                    return Box::pin(async { Err(UnauthorizedError::default().into()) });
                 }
             }
         } else {
             println!("Path is not secured");
         }
-                
+
         let fut = self.service.call(req);
 
         Box::pin(async move {
@@ -134,9 +144,7 @@ where
             Ok(res)
         })
     }
-    
 }
-
 
 impl<S, B, AuthProvider, U> Transform<S, ServiceRequest> for AuthMiddleware<AuthProvider, U>
 where
@@ -144,7 +152,7 @@ where
     S::Future: 'static,
     B: 'static,
     AuthProvider: GetAuthenticatedUserFromRequest<U> + Clone,
-    U: DeserializeOwned + 'static
+    U: DeserializeOwned + 'static,
 {
     type Response = ServiceResponse<B>;
     type Error = Error;
@@ -153,11 +161,11 @@ where
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(AuthMiddlewareInner { 
+        ready(Ok(AuthMiddlewareInner {
             service,
             path_matcher: Rc::clone(&self.path_matcher),
             auth_provider: Rc::clone(&self.auth_provider),
-            user_type: PhantomData, 
+            user_type: PhantomData,
         }))
     }
 }
@@ -187,5 +195,4 @@ mod tests {
         assert!(matcher.matches("/api/users/231/edit"));
         assert!(!matcher.matches("/login"));
     }
-
 }
