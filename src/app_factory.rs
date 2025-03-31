2f0c9be85bfe750e3694838c51853b8ad9ff11dc
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-03-31 20:02:20 +0200

    [*] Define routes for login

--- src/app_factory.rs
+++ src/app_factory.rs
@@ -2,8 +2,8 @@ use std::sync::Arc;
 
 use actix_files::Files;
 use actix_session::{config::{PersistentSession, SessionLifecycle}, storage::CookieSessionStore, SessionMiddleware};
-use actix_web::{body::MessageBody, cookie::Key, dev::{ServiceFactory, ServiceRequest, ServiceResponse}, get, http::StatusCode, web::Data, App, Error, HttpResponse, Responder};
-use authfix::{middleware::{AuthMiddleware, PathMatcher}, session::{handlers::SessionLoginHandler, session_auth::{session_login_factory, SessionAuthProvider}}};
+use actix_web::{body::MessageBody, cookie::Key, dev::{ServiceFactory, ServiceRequest, ServiceResponse}, get, web::Data, App, Error, HttpResponse, Responder};
+use authfix::{config::Routes, middleware::{AuthMiddleware, PathMatcher}, session::{handlers::SessionLoginHandler, session_auth::{session_login_factory, SessionAuthProvider}}};
 use serde::Serialize;
 
 use crate::{controller::activity_controller, domain::user_api::UserApi, service::{auth_service::AuthenticationService, user_service::UserService}};
@@ -43,8 +43,11 @@ impl ServiceFactory<
     let user_api: Arc<dyn UserApi> = Arc::clone(&user_service) as Arc<dyn UserApi>;
     let user_api_data = Data::from(user_api);
 
+    let mut login_handler = SessionLoginHandler::new(AuthenticationService::new(Arc::clone(&user_service)));
+    login_handler.set_routes(Routes::new("/api", "/login", "/login/mfa", "/logout"));
+
     session_login_factory(
-        SessionLoginHandler::new(AuthenticationService::new(Arc::clone(&user_service))), 
+        login_handler,
         AuthMiddleware::new(SessionAuthProvider, PathMatcher::new(vec!["/api/login/*","/api/test", "/web/index.html"], true)), 
         create_test_session_middleware(cookie_key)
     )
