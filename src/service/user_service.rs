a60acb243bbc9872850bd863b93a6bf33d2c9ce4
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-04-15 23:43:49 +0200

    [FIX] Save and load credentials

--- src/service/user_service.rs
+++ src/service/user_service.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use rusqlite::Connection;
 
-use crate::{config::db::DbConfig, domain::{user::{Credentials, User}, user_api::UserApi}, error::errors::{QueryUserError, UserUpdateError}};
+use crate::{config::db::DbConfig, domain::{user::{Credentials, MfaConfig, User}, user_api::UserApi}, error::errors::{QueryUserError, UserUpdateError}};
 
 pub struct UserService {
     db_config: Arc<DbConfig>
@@ -99,14 +99,16 @@ impl UserApi for UserService {
                 None => todo!(),
             };
 
-            let command: &str = match credentials.id > 0 {
-                true => "UPDATE credentials SET password = ?1, mfa_id = ?2, mfa_secret = ?3 WHERE id = ?4",
-                false => "INSERT INTO credentials (password, mfa_id, mfa_secret)",
+            let command = match credentials.id > 0 {
+                true => ("UPDATE credentials SET password = ?1, mfa_id = ?2, mfa_secret = ?3 WHERE id = ?4", 
+                    (credentials.password, mfa_config.0, mfa_config.1, credentials.id)),
+                false => ("INSERT INTO credentials (password, mfa_id, mfa_secret, user_id) values (?1, ?2, ?3, ?4)", 
+                    (credentials.password, mfa_config.0, mfa_config.1, credentials.user_id)),
             };
 
             let exec: Result<(), rusqlite::Error> = tokio::task::spawn_blocking(move || {
-                let conn = Connection::open(db)?;    
-                conn.execute(command, (credentials.password, mfa_config.0, mfa_config.1))?;
+                let conn: Connection = Connection::open(db)?;    
+                conn.execute(command.0, command.1)?;
 
                 Ok::<(), rusqlite::Error>(())
             }).await?;
@@ -114,7 +116,7 @@ impl UserApi for UserService {
             match exec {
                 Ok(_) => self.find_credentials_by_user_id(credentials.user_id).await
                 .map_err(|e| UserUpdateError::new(&format!("Cannot load credentials after save: {}", e))),
-                Err(_) => Err(UserUpdateError::new("Cannot insert or update credentials")),
+                Err(e) => Err(UserUpdateError::new(&format!("Cannot insert or update credentials: {}", e))),
             }
         }   
     }
@@ -124,9 +126,66 @@ impl UserApi for UserService {
         tokio::task::spawn_blocking(move || {
             let conn = Connection::open(db)?;
 
-            Ok(conn.query_row("SELECT id, password, user_id FROM credentials WHERE user_id = ?1", [user_id], |row| {
-                Ok(Credentials::new(row.get(0)?, row.get(1)?, row.get(2)?))
+            Ok(conn.query_row("SELECT id, password, mfa_id, mfa_secret, user_id FROM credentials WHERE user_id = ?1", [user_id], |row| {
+                let mfa_id: Option<String> = row.get(2)?;
+                let mfa_secret: Option<String> = row.get(3)?;
+
+                let mut mfa_config = None;
+                if let Some(mfa_id) = mfa_id {
+                    if let Some(mfa_secret) = mfa_secret {
+                        mfa_config = Some(MfaConfig::with_secret(&mfa_id, &mfa_secret));
+                    } else {
+                        mfa_config = Some(MfaConfig::new(&mfa_id));
+                    }
+                }
+                
+                let mut creds = Credentials::new(row.get(0)?, row.get(1)?, row.get(4)?);
+                if let Some(mfa_config) = mfa_config {
+                    creds.set_mfa(mfa_config);
+                }
+                Ok(creds)
             })?)
         }).await?
     }
+}
+
+#[cfg(test)]
+mod user_service_tests {
+    use std::{fs::remove_file, sync::Arc};
+
+    use crate::{config::db::DbConfig, create_db, domain::{user::{MfaConfig, User}, user_api::UserApi}, service::user_service::UserService};
+
+
+    #[tokio::test]
+    async fn should_be_able_to_save_credentials() {
+        // ToDo: user in memory config
+        let temp_db = "should_be_able_to_save_credentials.sqlite3";
+        let db_config = DbConfig::new(temp_db);
+        create_db(&db_config);
+
+        // Arrange
+        let user_service = UserService::new(Arc::new(db_config));
+        let user = User::new(0, "test@example.org".to_owned(), "Test User".to_owned());
+        let saved_user = user_service.save_user_with_credentials(user, "secretpassword").await.unwrap();
+
+
+        // Act
+        let mut creds = user_service.find_credentials_by_user_id(saved_user.id).await.unwrap();
+        creds.set_mfa(MfaConfig::with_secret("MFA_ID", "asecret"));
+        user_service.save_credentials(creds).await.unwrap();
+
+        // Assert
+        creds = user_service.find_credentials_by_user_id(saved_user.id).await.unwrap();
+
+        // clean db
+        remove_file(temp_db).unwrap();
+
+        assert!(creds.mfa_config.is_some());
+        let mfa_config = creds.mfa_config.unwrap();
+        assert_eq!(mfa_config.mfa_id, "MFA_ID");
+        assert!(mfa_config.secret.is_some());
+        assert_eq!(mfa_config.secret.unwrap(), "asecret");
+    }
+
+
 }
